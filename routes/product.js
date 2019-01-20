7e84f08688c1c4079c7851917c3fd4b06018a58e
freebooz <365256100@qq.com>
2019-01-20 14:23:48 +0800

    1

---
+++ routes/product.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var db = require("../lib/db.js");
+
+/* 获取商品类目列表. */
+router.get('/cate/list', function (req, res, next) {
+    var sql = "select * from base_product_cate";
+    db.query(sql, function (err, data) {
+        res.send(data);
+    });
+});
+
+/* 获取商品列表. */
+router.get('/list', function (req, res, next) {
+    var sql = "select * from base_product";
+    db.query(sql, function (err, data) {
+        res.send(data);
+    });
+});
+
+module.exports = router;
